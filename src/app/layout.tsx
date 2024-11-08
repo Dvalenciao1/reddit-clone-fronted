ceb76b2743fc902d8b01e7e7b6f9ce50499c4ce1
Dvalenciao1 <dv237968@gmail.com>
2024-11-08 13:35:43 -0500

    (Feat) scrollbar: add style for scrollbar on app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import NavBar from "../components/navbar/NavBar";
-import { Box, CssBaseline, ThemeProvider, Toolbar } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "@/theme/theme";
+import "@/Global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,11 +21,10 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<ThemeProvider theme={theme}>
-				<CssBaseline />
-
+			<CssBaseline enableColorScheme />
 				<body className={inter.className}>
 					<NavBar />
-					<Box component="main" sx={{ flexGrow: 2, pl:"18rem", pt:7 }}>
+					<Box component="main" sx={{ flexGrow: 2, pl: "18rem", pt: 7 }}>
 						{children}
 					</Box>
 				</body>
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,14 @@
-'use client'
-import { createTheme } from '@mui/material/styles';
+"use client";
+import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
-    palette: {
-        mode : "dark",
-        primary: {
-            main: "#FF4F40",
-        },
-        secondary: {
-            main: "#3d494e",
-        },
-    }
-})
+	palette: {
+		mode: "dark",
+		primary: {
+			main: "#FF4F40",
+		},
+		secondary: {
+			main: "#3d494e",
+		},
+	}
+});
