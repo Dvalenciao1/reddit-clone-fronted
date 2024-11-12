361430258f9ee3a66305c4f0f93bab37d8871db4
Dvalenciao1 <dv237968@gmail.com>
2024-11-11 19:02:45 -0500

    feat(login): add dialog login and fields

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
-
-import { Button, Dialog } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, IconButton, TextField } from "@mui/material";
+import ClearRoundedIcon from "@mui/icons-material/ClearRounded";
 import React from "react";
 
 export default function LoginForm() {
@@ -13,13 +13,39 @@ export default function LoginForm() {
 	};
 
 	return (
-		<div>
+		<>
 			<Button color="primary" variant="contained" sx={{ borderRadius: 5 }} onClick={handleClickOpen}>
 				Login
 			</Button>
-			<Dialog open={open} onClose={handleClose}>
-				awd
+			<Dialog fullWidth={true} maxWidth={"sm"} open={open} onClose={handleClose}>
+				<DialogActions>
+					<IconButton>
+						<ClearRoundedIcon />
+					</IconButton>
+				</DialogActions>
+				<DialogContent sx={{ px: "80px" }}>
+					<DialogTitle>Login</DialogTitle>
+					<DialogContent>
+						<DialogContentText>By continuing, you agree to out User Agreement and acknowledge that you understand the Privacy Policy</DialogContentText>
+						<div className="flex flex-col py-5">
+							<Button className="mb-[6px] rounded-full bg-slate-100 text-slate-900" variant="contained">
+								Continue with Google
+							</Button>
+							<Button className="mt-[6px] rounded-full bg-slate-100 text-slate-900" variant="contained">
+								Continue with Apple
+							</Button>
+						</div>
+						<Divider>Or</Divider>
+						<div className="py-5">
+							<TextField fullWidth className="mb-3" slotProps={{ input: { style: { borderRadius: "16px" } } }} label="Email or Username"/>
+							<TextField fullWidth className="mt-3" slotProps={{ input: { style: { borderRadius: "16px" } } }} label="Password"/>
+						</div>
+					</DialogContent>
+				</DialogContent>
+				<DialogActions>
+					<Button type="submit">Login</Button>
+				</DialogActions>
 			</Dialog>
-		</div>
+		</>
 	);
 }
