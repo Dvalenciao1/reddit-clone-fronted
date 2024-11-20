ddf4bdcb414d13298f7ab1c2b8b14da57f5c03d2
Dvalenciao1 <dv237968@gmail.com>
2024-11-20 12:06:00 -0500

    feat(carousel): implementing buttons to next, back and modify styles

--- src/components/post/carousel.tsx
+++ src/components/post/carousel.tsx
@@ -1,23 +1,24 @@
-import { Avatar, Card, CardActionArea, CardContent, CardMedia, Stack, Typography } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import { ImageCarousel } from "./ImageCarousel";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function Carousel() {
 	return (
 		<>
-			<Card>
-				<CardActionArea sx={{ position: "relative", width: "280px" }}>
-					<CardMedia sx={{ position: "absolute" }} component="img" image="static/iguana.jpg" alt="green iguana" />
-					<CardContent sx={{ position: "relative", height: "210px", display: "flex", flexDirection: "column", justifyContent: "end" }}>
-						<Typography component="h2" sx={{ fontWeight: "700", fontSize: "18px" }}>
-							Chargers defeat Bengals
-						</Typography>
-						<Typography component="p" variant="caption" sx={{ fontSize: "14px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", margin: "4px 0 8px" }}>
-							[Highlitght] Chargers game winning drive
-						</Typography>
-						<Avatar sx={{ width: 24, height: 24, bgcolor: "primary.main" }}></Avatar>
-					</CardContent>
-				</CardActionArea>
-			</Card>
+			<Box sx={{ position: "relative" }} className="grid grid-flow-col gap-[0.5rem] remove-scrollbar overflow-x-auto overscroll-x-contain snap-x snap-mandatory scroll-auto">
+				[
+				<IconButton size="small" sx={{ "position": "absolute", "top": "50%", "left": "3.5%", "zIndex": 1, "transform": "translate(0, -80%)", "backgroundColor": "#38393A ", "opacity": "0.8", "&:hover": { backgroundColor: "#1D1E1E" } }}>
+					<ArrowBackIcon />
+				</IconButton>
+				]
+				{[...Array(10)].map((_, i) => (
+					<ImageCarousel key={i}></ImageCarousel>
+				))}
+				<IconButton size="small" sx={{ "position": "absolute", "top": "50%", "left": "100%", "zIndex": 1, "transform": "translate(-40%, -80%)", "backgroundColor": "#38393A ", "opacity": "0.8", "&:hover": { backgroundColor: "#1D1E1E" } }}>
+					<ArrowForwardIcon />
+				</IconButton>
+			</Box>
 		</>
 	);
 }
