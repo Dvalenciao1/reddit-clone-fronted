42bdf09394450c4552cd99e533b1a51fc35a604f
Dvalenciao1 <dv237968@gmail.com>
2024-11-20 19:17:40 -0500

    feat(carousel): implementing swiper for carousel

--- src/components/post/carousel.tsx
+++ src/components/post/carousel.tsx
@@ -4,25 +4,26 @@ import { ImageCarousel } from "./ImageCarousel";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+
 export default function Carousel() {
-	function handledClickNext() {
-		const container = document.getElementById("#container-carousel");
-		if (container) {
-			container.scrollLeft += 100;
-		}
-	}
-
-	function handledClickPrev() {
-		const container = document.getElementById("#container-carousel");
-		if (container) {
-			container.scrollLeft -= 100;
-		}
-	}
-
 	return (
 		<>
-			<Box sx={{ position: "relative" }} component="div">
-				<IconButton onClick={handledClickPrev} size="small" sx={{ "position": "absolute", "top": "50%", "left": "1%", "zIndex": 1, "transform": "translate(0, -80%)", "backgroundColor": "#38393A ", "opacity": "0.8", "&:hover": { backgroundColor: "#1D1E1E" } }}>
+			<Swiper navigation={true} modules={[Navigation]} spaceBetween={70} slidesPerView={4}>
+				{[...Array(10)].map((_, i) => (
+					<SwiperSlide key={i}>
+						<ImageCarousel></ImageCarousel>
+					</SwiperSlide>
+				))}
+			</Swiper>
+			{/* <Box sx={{ position: "relative" }} component="div">
+				<IconButton size="small" sx={{ "position": "absolute", "top": "50%", "left": "1%", "zIndex": 1, "transform": "translate(0, -80%)", "backgroundColor": "#38393A ", "opacity": "0.8", "&:hover": { backgroundColor: "#1D1E1E" } }}>
 					<ArrowBackIcon />
 				</IconButton>
 				<Box id="container-carousel" className="grid grid-flow-col gap-[0.5rem] remove-scrollbar overflow-x-auto overscroll-x-contain snap-x snap-mandatory scroll-auto">
@@ -31,10 +32,10 @@ export default function Carousel() {
 					))}
 				</Box>
 
-				<IconButton onClick={handledClickNext} size="small" sx={{ "position": "absolute", "top": "50%", "left": "100%", "zIndex": 1, "transform": "translate(-115%, -80%)", "backgroundColor": "#38393A ", "opacity": "0.8", "&:hover": { backgroundColor: "#1D1E1E" } }}>
+				<IconButton size="small" sx={{ "position": "absolute", "top": "50%", "left": "100%", "zIndex": 1, "transform": "translate(-115%, -80%)", "backgroundColor": "#38393A ", "opacity": "0.8", "&:hover": { backgroundColor: "#1D1E1E" } }}>
 					<ArrowForwardIcon />
 				</IconButton>
-			</Box>
+			</Box> */}
 		</>
 	);
 }
