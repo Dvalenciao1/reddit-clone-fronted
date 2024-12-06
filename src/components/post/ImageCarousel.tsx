c4c060ca8d8a81738fc08870db00aea137612c36
Dvalenciao1 <dv237968@gmail.com>
2024-12-06 13:21:25 -0500

    fix(carousel): modify thumbs on carousel and changes styles

--- src/components/post/ImageCarousel.tsx
+++ src/components/post/ImageCarousel.tsx
@@ -6,11 +6,11 @@ export function ImageCarousel() {
 			<Card sx={{ minWidth: "280px", borderRadius: "16px", scrollSnapAlign: "start" }}>
 				<CardActionArea sx={{ position: "relative", width: "280px" }}>
 					<CardMedia sx={{ position: "absolute" }} component="img" image="static/iguana.jpg" alt="green iguana" />
-					<CardContent sx={{ position: "relative", height: "210px", display: "flex", flexDirection: "column", justifyContent: "end" }}>
+					<CardContent sx={{ position: "relative", height: "210px", display: "flex", flexDirection: "column", justifyContent: "end", py: "5px" }}>
 						<Typography component="h2" sx={{ fontWeight: "700", fontSize: "18px" }}>
 							Chargers defeat Bengals
 						</Typography>
-						<Typography component="p" variant="caption" sx={{ fontSize: "14px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", margin: "4px 0 8px" }}>
+						<Typography component="p" variant="caption" sx={{ fontSize: "14px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", pb: "3px" }}>
 							[Highlitght] Chargers game winning drive
 						</Typography>
 						<Avatar sx={{ width: 24, height: 24, bgcolor: "primary.main" }}></Avatar>
