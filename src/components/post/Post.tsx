f17b65603ae63079f523537006eac208cd99e129
Dvalenciao1 <dv237968@gmail.com>
2024-12-09 20:02:15 -0500

    feat(post): add new button for follow and options on posts cards and timestamp

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,18 +1,34 @@
-import { Avatar, Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import MapsUgcIcon from "@mui/icons-material/MapsUgc";
 import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
 import ShareIcon from "@mui/icons-material/Share";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import InteractionButton from "./InteractionButton";
 
 export default function FeedPost({ image }: { image: string }) {
 	return (
 		<>
-			<Card elevation={0} raised={false} sx={{ background: "#121212", maxWidth:"800px" }}>
+			<Card elevation={0} raised={false} sx={{ background: "#121212", maxWidth: "800px" }}>
 				<Box className="rounded-[32px] hover:bg-[#252525] cursor-pointer">
-					<CardContent className="mx-1">
-						<Avatar className="my-1" sx={{ width: 24, height: 24 }}></Avatar>
+					<CardContent className="mx-1" sx={{ mx: 1, py: 0 }}>
+						<Box className="flex justify-between">
+							<Box sx={{ display: "flex", alignItems: "center" }}>
+								<Avatar sx={{ width: 24, height: 24, my: 1 }}></Avatar>
+								<Typography sx={{ mx: 2 }} variant="caption" color="text.secondary">
+									r/aww • hace 8 h
+								</Typography>
+							</Box>
+							<Box sx={{ display: "flex", alignItems: "center" }}>
+								<Button sx={{ "mx": 1, "bgcolor": "#115BCA", "borderRadius": "999px", "height": "24px", "textTransform": "capitalize", ":hover": { bgcolor: "#1870F4" } }} variant="contained">
+									Unirse
+								</Button>
+								<IconButton sx={{ mx: 1, maxWidth: "32px", maxHeight: "32px" }}>
+									<MoreHorizIcon />
+								</IconButton>
+							</Box>
+						</Box>
 						<Typography gutterBottom variant="h5" component="div">
 							Lizard
 						</Typography>
